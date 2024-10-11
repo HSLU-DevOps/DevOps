794928acc047bd648e4f907b2fa926f08a2955e9
Ilija <ilija.milovanovic@stud.hslu.ch>
2024-10-11 16:51:28 +0200

    init

---
+++ WebAPI/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TodoDbContext>(opt =>
+{
+    opt.UseNpgsql($"User ID={Environment.GetEnvironmentVariable("POSTGRES_USER")};" +
+                  $"Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")};" +
+                  $"Host=postgres;" +  // Use 'postgres' to reference the Docker service
+                  $"Port=5432;" +
+                  $"Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};" +
+                  $"Pooling=true;" +
+                  $"Min Pool Size=0;" +
+                  $"Max Pool Size=100;" +
+                  $"Connection Lifetime=0;");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
