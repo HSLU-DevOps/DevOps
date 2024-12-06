5952931a458429e32d4d5a4678c35951f08a7f42
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2024-12-06 10:54:15 +0100

    feat: expose application metrics

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.FeatureManagement;
+using OpenTelemetry.Metrics;
 using WebAPI.Controllers;
 using WebAPI.Persistence;
 
@@ -26,6 +27,17 @@ builder.Services.AddDbContext<TodoDbContext>(opt =>
 });
 
 builder.Services.AddFeatureManagement();
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+        builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
+            });
+    });
 
 var app = builder.Build();
 
@@ -48,5 +60,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapHealthChecks("/health");
+
+app.MapPrometheusScrapingEndpoint();
 
 app.Run();
