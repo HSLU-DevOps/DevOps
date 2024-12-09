9273b15244087bb4dc25c4f35d9fa629b3636e0f
SomeFunnyDev <ilija.milovanovic@stud.hslu.ch>
2024-12-09 23:47:54 +0100

    clean up

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -28,11 +28,11 @@ builder.Services.AddDbContext<TodoDbContext>(opt =>
 
 builder.Services.AddFeatureManagement();
 builder.Services.AddOpenTelemetry()
-    .WithMetrics(builder =>
+    .WithMetrics(telemetryBuilder =>
     {
-        builder.AddPrometheusExporter();
-        builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
-        builder.AddView("http.server.request.duration",
+        telemetryBuilder.AddPrometheusExporter();
+        telemetryBuilder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
+        telemetryBuilder.AddView("http.server.request.duration",
             new ExplicitBucketHistogramConfiguration
             {
                 Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
